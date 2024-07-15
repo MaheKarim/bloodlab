d4b00c71bfe41d249fbce0827903a70e710d04e4
Mahe Karim <mahekarim@gmail.com>
2024-07-15 22:19:04 +0600

    Dashboard Half WIP :rocket:

--- core/resources/views/admin/dashboard.blade.php
+++ core/resources/views/admin/dashboard.blade.php
@@ -5,319 +5,90 @@
     <div class="row gy-4">
 
         <div class="col-xxl-3 col-sm-6">
-
             <x-widget
                 style="6"
-                link=""
+                link="{{ route('admin.donor.index') }}"
                 icon="las la-users"
-                title="Total Users"
-                value=""
+                title="Total Donor"
+                value="{{ $donor['all'] }}"
                 bg="primary"
             />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="6"
-                link=""
-                icon="las la-user-check"
-                title="Active Users"
-                value=""
-                bg="success"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="6"
-                link=""
-                icon="lar la-envelope"
-                title="Email Unverified Users"
-                value=""
-                bg="danger"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="6"
-                link=""
-                icon="las la-comment-slash"
-                title="Mobile Unverified Users"
-                value=""
-                bg="warning"
-            />
-        </div><!-- dashboard-w1 end -->
-    </div><!-- row end-->
-
-
-
-    <div class="row gy-4 mt-2">
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="7"
-                link="#"
-                title="Demo"
-                icon="far fa-user"
-                value="xxx"
-                bg="success"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="7"
-                link="#"
-                title="Demo"
-                icon="far fa-user"
-                value="xxx"
-                bg="warning"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="7"
-                link="#"
-                title="Demo"
-                icon="far fa-user"
-                value="xxx"
-                bg="danger"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="7"
-                link="#"
-                title="Demo"
-                icon="far fa-user"
-                value="xxx"
-                bg="primary"
-            />
-        </div><!-- dashboard-w1 end -->
-    </div><!-- row end-->
-
-    <div class="row gy-4 mt-2">
-        <div class="col-xxl-2 col-lg-4 col-sm-6">
-            <x-widget
-                style="5"
-                link="#"
-                icon="las la-users"
-                title="Demo"
-                value="xxx"
-                bg="primary"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-2 col-lg-4 col-sm-6">
-            <x-widget
-                style="5"
-                link="#"
-                icon="las la-users"
-                title="Demo"
-                value="xxx"
-                bg="1"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-2 col-lg-4 col-sm-6">
-            <x-widget
-                style="5"
-                link="#"
-                icon="las la-users"
-                title="Demo"
-                value="xxx"
-                bg="2"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-2 col-lg-4 col-sm-6">
-            <x-widget
-                style="5"
-                link="#"
-                icon="las la-users"
-                title="Demo"
-                value="xxx"
-                bg="3"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-2 col-lg-4 col-sm-6">
-            <x-widget
-                style="5"
-                link="#"
-                icon="las la-users"
-                title="Demo"
-                value="xxx"
-                bg="4"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-2 col-lg-4 col-sm-6">
-            <x-widget
-                style="5"
-                link="#"
-                icon="las la-users"
-                title="Demo"
-                value="xxx"
-                bg="5"
-            />
-        </div><!-- dashboard-w1 end -->
-    </div><!-- row end-->
-
-    <div class="row gy-4 mt-2">
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="6"
-                link="#"
-                title="Demo"
-                icon="far fa-user"
-                value="xxx"
-                bg="success"
-                outline="true"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="6"
-                link="#"
-                title="Demo"
-                icon="far fa-user"
-                value="xxx"
-                bg="warning"
-                outline="true"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="6"
-                link="#"
-                title="Demo"
-                icon="far fa-user"
-                value="xxx"
-                bg="danger"
-                outline="true"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="6"
-                link="#"
-                title="Demo"
-                icon="far fa-user"
-                value="xxx"
-                bg="primary"
-                outline="true"
-            />
-        </div><!-- dashboard-w1 end -->
-    </div><!-- row end-->
-
-    <div class="row gy-4 mt-2">
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="3"
-                link="#"
-                icon="las la-users"
-                title="Demo"
-                value="xxx"
-                bg="primary"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="3"
-                link="#"
-                icon="las la-users"
-                title="Demo"
-                value="xxx"
-                bg="1"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="3"
-                link="#"
-                icon="las la-users"
-                title="Demo"
-                value="xxx"
-                bg="14"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="3"
-                link="#"
-                icon="las la-users"
-                title="Demo"
-                value="xxx"
-                bg="19"
-            />
-        </div><!-- dashboard-w1 end -->
-    </div><!-- row end-->
-
-    <div class="row gy-4 mt-2">
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="7"
-                link="#"
-                title="Demo"
-                icon="far fa-user"
-                value="xxx"
-                bg="success"
-                type="2"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="7"
-                link="#"
-                title="Demo"
-                icon="far fa-user"
-                value="xxx"
-                bg="warning"
-                type="2"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="7"
-                link="#"
-                title="Demo"
-                icon="far fa-user"
-                value="xxx"
-                bg="danger"
-                type="2"
-            />
-        </div><!-- dashboard-w1 end -->
-        <div class="col-xxl-3 col-sm-6">
-            <x-widget
-                style="7"
-                link="#"
-                title="Demo"
-                icon="far fa-user"
-                value="xxx"
-                bg="primary"
-                type="2"
-            />
-        </div><!-- dashboard-w1 end -->
-    </div>
-
-    <div class="row mb-none-30 mt-5">
-        <div class="col-xl-4 col-lg-6 mb-30">
-            <div class="card overflow-hidden">
-                <div class="card-body">
-                    <h5 class="card-title">@lang('Login By Browser') (@lang('Last 30 days'))</h5>
-                    <canvas id="userBrowserChart"></canvas>
-                </div>
-            </div>
         </div>
-        <div class="col-xl-4 col-lg-6 mb-30">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">@lang('Login By OS') (@lang('Last 30 days'))</h5>
-                    <canvas id="userOsChart"></canvas>
-                </div>
-            </div>
+        <div class="col-xxl-3 col-sm-6">
+            <x-widget
+                style="6"
+                link="{{ route('admin.donor.pending') }}"
+                icon="las la-spinner"
+                title="Total Pending Donor"
+                value="{{ $donor['pending'] }}"
+                bg="warning"
+            />
         </div>
-        <div class="col-xl-4 col-lg-6 mb-30">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">@lang('Login By Country') (@lang('Last 30 days'))</h5>
-                    <canvas id="userCountryChart"></canvas>
-                </div>
-            </div>
+        <div class="col-xxl-3 col-sm-6">
+            <x-widget
+                style="6"
+                link="{{route('admin.donor.approved')}}"
+                icon="las la-user-slash"
+                title="Total Banned Donor"
+                value="{{ $donor['banned'] }}"
+                bg="danger"
+            />
         </div>
-    </div>
+        <div class="col-xxl-3 col-sm-6">
+            <x-widget
+                style="6"
+                link="{{ route('admin.donor.banned') }}"
+                icon="las la-user-plus"
+                title="Total Approved Donor"
+                value="{{ $donor['approved'] }}"
+                bg="success"
+            />
+        </div>
+    </div><!-- row end-->
+
+    <div class="row gy-4 mt-2">
+        <div class="col-xxl-3 col-sm-6">
+            <x-widget
+                style="6"
+                link="{{route('admin.blood-group.index')}}"
+                title="Total Blood Group"
+                icon="las la-tint"
+                value="{{ $blood }}"
+                bg="success"
+            />
+        </div><!-- dashboard-w1 end -->
+        <div class="col-xxl-3 col-sm-6">
+            <x-widget
+                style="6"
+                link="{{route('admin.city.index')}}"
+                title="Total City"
+                icon="las la-city"
+                value="{{ __($city) }}"
+                bg="info"
+            />
+        </div><!-- dashboard-w1 end -->
+        <div class="col-xxl-3 col-sm-6">
+            <x-widget
+                style="6"
+                link="{{route('admin.location.index')}}"
+                title="Total Location"
+                icon="las la-map"
+                value="{{__($locations)}}"
+                bg="secondary"
+            />
+        </div><!-- dashboard-w1 end -->
+        <div class="col-xxl-3 col-sm-6">
+            <x-widget
+                style="6"
+                link="#"
+                title="Total Advertisement"
+                icon="las la-ad"
+                value="{{__($ads)}}"
+                bg="dark"
+            />
+        </div><!-- dashboard-w1 end -->
+    </div><!-- row end-->
+
 
 @endsection
 
